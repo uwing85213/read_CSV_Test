a00543947c3647e1f07e8ded2b0d7aa4937143c0
YingRen_Lin <uwing85213@gmail.com>
2024-05-21 22:23:30 +0800

    能產4張圖
    Signed-off-by: YingRen_Lin <uwing85213@gmail.com>

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 
-from google.colab import drive
-drive.mount('/content/drive')
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 # with open("/content/drive/MyDrive/FN2_test/#1.csv", newline='') as csvfile:
 
@@ -12,34 +12,54 @@ drive.mount('/content/drive')
 #   for row in rows:
     # print(row)
 
+# Parameter
+Heartbit_Data_Index = 58
+
+OGH_Bit_Data_Index = 49
+
+Fan1_RPM_Index = 17
+Fan2_RPM_Index = 21
+
+Intel_PL4_Index = 13
+Intel_PL2_Index = 8
+Intel_PL1_Index = 3
 
 
-data = pd.read_csv("/content/drive/MyDrive/FN2_test/#1.csv")
+SaveDir_Path = "data/img/"
 
-len(data)
-
-hearbit = data.iloc[:,58] #Heartbit data
-
-plt.figure(figsize=(20,5))
-plt.plot(hearbit, color='blue')
+figsize_setting = (7,5)
 
 
-data_2 = data.iloc[:,49] #OGH_Bit data
-#plt.figure(figsize=(20,5))
-plt.plot(data_2, color='blue')
+data = pd.read_csv("data/CSV_data/#3.csv")
 
+plt.figure(figsize=figsize_setting)#, dpi=dpin
+Heartbit_data = data.iloc[:,Heartbit_Data_Index] #Heartbit data
+plt.plot(Heartbit_data, color='blue',linestyle='-', label="Heartbit")
+plt.title("Heartbit")
+plt.savefig(SaveDir_Path+"_HB.png")
 
-data_2 = data.iloc[:,17] #RPM data
-data_3 = data.iloc[:,21] #RPM data
-#plt.figure(figsize=(20,5))
-plt.plot(data_2, color='blue')
-plt.plot(data_3, color='red')
+plt.figure(figsize=figsize_setting) 
+data_2 = data.iloc[:,OGH_Bit_Data_Index] #OGH_Bit data
+plt.plot(data_2, color='blue', label="Sku")
+plt.title("Fan Ctrl")
+plt.savefig(SaveDir_Path+"_OGH.png")
 
+plt.figure(figsize=figsize_setting)#, dpi=dpin
+Fan1_RPM_data = data.iloc[:,Fan1_RPM_Index] #RPM data
+Fan2_RPM_data = data.iloc[:,Fan2_RPM_Index] #RPM data
+plt.plot(Fan1_RPM_data, color='blue',label="Fan1_RPM" )
+plt.plot(Fan2_RPM_data, color='orange',label="Fan2_RPM")
+plt.title("FAN RPM")
+plt.legend()
+plt.savefig(SaveDir_Path+"_RPM.png")
 
-data_4 = data.iloc[:,3] #PL1 data
-data_5 = data.iloc[:,8] #PL2 data
-data_6 = data.iloc[:,13] #PL4 data
-#plt.figure(figsize=(10,10))
-plt.plot(data_4, color='blue' ,label='PL1')
-plt.plot(data_5, color='red' ,label='PL2')
-plt.plot(data_6, color='green',label='PL4')
+plt.figure(figsize=figsize_setting)#, dpi=dpin
+Intel_PL1_data = data.iloc[:,Intel_PL1_Index] #PL1 data
+Intel_PL2_data = data.iloc[:,Intel_PL2_Index] #PL2 data
+Intel_PL4_data = data.iloc[:,Intel_PL4_Index] #PL4 data
+plt.plot(Intel_PL4_data, color='gray',label='PL4')
+plt.plot(Intel_PL2_data, color='orange' ,label='PL2')
+plt.plot(Intel_PL1_data, color='blue' ,label='PL1')
+plt.title("Sku name")
+plt.legend()
+plt.savefig(SaveDir_Path+"_PLx.png")
