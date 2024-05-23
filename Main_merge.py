69273919ea9faa6d7d433ae2d55cadf441fb2d3e
YingRen_Lin <YingRen_Lin@compal.com>
2024-05-23 10:40:33 +0800

    新增Y軸刻度與虛線
    Change-Id: I4ba8b7602e7749ee60fac587d1b9231665091415

--- Main_merge.py
+++ Main_merge.py
@@ -108,17 +108,21 @@ if len(csv_files) >0 :
         create_or_check_directory(SaveDir_Path + file_tile + "/" )
         SaveDir_Path_new =  SaveDir_Path + file_tile + "/"
 
-        # data = pd.read_csv( csv_file )
-        data = read_csv_with_detected_encoding( csv_file )
+        data = pd.read_csv( csv_file )
+        #data = read_csv_with_detected_encoding( csv_file )
         
         plt.figure(figsize=figsize_setting)#, dpi=dpin
         Heartbit_data = data.iloc[:,Heartbit_Data_Index] #Heartbit data
+        plt.ylim(-10, 1400) #ylabel = 0~6
+        plt.grid(axis='y', linestyle='--',color='gray', linewidth=1)
         plt.plot(Heartbit_data, color='blue',linestyle='-', label="Heartbit")
         plt.title("Heartbit")
         plt.savefig(SaveDir_Path_new + file_tile +"_HB.png")
 
         plt.figure(figsize=figsize_setting) 
         data_2 = data.iloc[:,OGH_Bit_Data_Index] #OGH_Bit data
+        plt.ylim(-0.1, 6) #ylabel = 0~6
+        plt.grid(axis='y', linestyle='--')
         plt.plot(data_2, color='blue', label="Sku")
         plt.title("Fan Ctrl")
         plt.savefig(SaveDir_Path_new + file_tile +"_OGH.png")
@@ -126,6 +130,8 @@ if len(csv_files) >0 :
         plt.figure(figsize=figsize_setting)#, dpi=dpin
         Fan1_RPM_data = data.iloc[:,Fan1_RPM_Index] #RPM data
         Fan2_RPM_data = data.iloc[:,Fan2_RPM_Index] #RPM data
+        plt.ylim(0, 70) #ylabel = 0~70
+        plt.grid(axis='y', linestyle='--')
         plt.plot(Fan1_RPM_data, color='blue',label="Fan1_RPM" )
         plt.plot(Fan2_RPM_data, color=orange_color,label="Fan2_RPM")
         plt.title("FAN RPM")
@@ -136,6 +142,8 @@ if len(csv_files) >0 :
         Intel_PL1_data = data.iloc[:,Intel_PL1_Index] #PL1 data
         Intel_PL2_data = data.iloc[:,Intel_PL2_Index] #PL2 data
         Intel_PL4_data = data.iloc[:,Intel_PL4_Index] #PL4 data
+        plt.ylim(-1, 250) #ylabel = 0~6
+        plt.grid(axis='y', linestyle='--')
         plt.plot(Intel_PL4_data, color='gray',label='PL4')
         plt.plot(Intel_PL1_data, color='blue' ,label='PL1')
         plt.plot(Intel_PL2_data, color=orange_color ,label='PL2')
@@ -147,6 +155,8 @@ if len(csv_files) >0 :
         IR_temp_data = data.iloc[:,IR_temp_Index] #PL1 data
         CPU_temp_data = data.iloc[:,CPU_temp_Index] #PL2 data
         VGA_temp_data = data.iloc[:,VGA_temp_Index] #PL4 data
+        plt.ylim(-1, 101) #ylabel = 0~6
+        plt.grid(axis='y', linestyle='--')
         plt.plot(IR_temp_data, color='blue',label='IR')
         plt.plot(CPU_temp_data, color=orange_color ,label='CPU')
         plt.plot(VGA_temp_data, color='gray' ,label='VGA')
